a0361906393c85118adc1540f4391db7ab5a041d
Nilesh Pandit <42546950+nileshpandit009@users.noreply.github.com>
2019-04-03 21:58:31 +0530

    Delete run.sh

--- fabric/run.sh
+++
@@ -1,7 +0,0 @@
-docker stack deploy -c fabric-zk.yml ctso
-docker stack deploy -c fabric-kafka.yml ctso
-docker stack deploy -c fabric-orderer.yml ctso
-docker stack deploy -c fabric-couchdb.yml ctso
-docker stack deploy -c fabric-peer.yml ctso
-docker stack deploy -c fabric-ca-run.yml ctso
-docker stack deploy -c fabric-cli.yml ctso
